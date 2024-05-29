db97fbf6b92d09c12606734345da2c4b39f26cd6
nono <nour98nour@gmail.com>
2024-05-29 07:05:36 +0300

    Refactor pascal_triangle function to use iterative approach

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,11 +4,27 @@ This module is used to print the pascal triangle
 I have used the recursive approach to solve the problem
 """
 
+# pasc = []
+#
+# def pascal_triangle(n=0):
+#     if n == 0:
+#         return []
+#     elif n == 1:
+#         pasc.append([1])
+#         return pasc
+#     else:
+#         pascal_triangle(n-1)
+#         pasc.append([1])
+#         for j in range(1, n-1):
+#             pasc[n - 1].append(pasc[n - 2][j-1]+pasc[n - 2][j])
+#         pasc[n-1].append(1)
+#         return pasc
+
 
 def pascal_triangle(n=0):
     """
     This function is used to generate the pascal triangle
-    
+
     Args:
         @n: the number of rows to be generated
     """
